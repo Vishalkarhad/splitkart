eff2e22bf2d1ebb2d5c6149a8e8f472c1c22679a
vishal karhad <vishalkarhad199@gmail.com>
2025-01-12 21:33:30 +0530

    start

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify,g
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from pymongo import MongoClient, ReturnDocument
 from werkzeug.security import generate_password_hash, check_password_hash
 import secrets
